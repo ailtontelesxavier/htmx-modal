4ddb67b1200e3bc631862d000ecc2abbdc164591
ailton <atx.shared@gmail.com>
2023-03-14 17:57:15 -0300

    alteracao staticroot

--- django_htmx_modal_form/settings.py
+++ django_htmx_modal_form/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.contrib import messages
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -127,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- movie_collection/models.py
+++ movie_collection/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse_lazy, reverse
+
+
+class Categoria(models.Model):
+
+    
+
+    class Meta:
+        verbose_name = ("Categoria")
+        verbose_name_plural = ("Categorias")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Categoria_detail", kwargs={"pk": self.pk})
+
 
 
 class Movie(models.Model):
 
     title = models.CharField(max_length=40, unique=True)
+    categoria = models.ForeignKey("Categoria", on_delete=models.CASCADE, blank=True, null=True)
 
     year = models.PositiveSmallIntegerField(
         validators=[
